9e1c75f4a586e8bf845e80d43097fb68783d1aab
Ken Yew Piong <zceekyp@ucl.ac.uk>
2019-12-12 19:51:29 +0000

    Added data-preprocessing functions

---
+++ AMLS_19-20_Ken_Yew_Piong_SN16004444/.ipynb_checkpoints/util-checkpoint.py
@@ -0,0 +1,14 @@
+def data_preprocessing(): 
+    return 1
+
+def build_model_task_A1():
+    return 1
+
+def build_model_task_A2():
+    return 1
+
+def build_model_task_B1():
+    return 1
+
+def build_model_task_B2():
+    return 1
