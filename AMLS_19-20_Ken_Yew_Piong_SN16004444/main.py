f19bfb781aa47b223df8e2268b902263dc5fa382
Ken Yew Piong <zceekyp@ucl.ac.uk>
2019-12-27 15:41:07 +0800

    Spring cleaning for submission

--- AMLS_19-20_Ken_Yew_Piong_SN16004444/main.py
+++ AMLS_19-20_Ken_Yew_Piong_SN16004444/main.py
@@ -1,43 +1,93 @@
-import ...
+import os, math, cv2, dlib, warnings, itertools
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from keras.preprocessing import image
+
+from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
+from sklearn.svm import SVC
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV, cross_val_score, learning_curve, train_test_split
+
+from util import *
+warnings.filterwarnings('ignore')
 
 # ======================================================================================================================
-# Data preprocessing
-data_train, data_val, data_test = data_preprocessing(args...)
+# Data preprocessing for Task A1 and A2
+sample_size = 5000 # Full dataset import
+landmark_features_celeba, gender_labels, smiling_labels = extract_features_labels_from_celeba(sample_size)
 # ======================================================================================================================
 # Task A1
-model_A1 = A1(args...)                 # Build model object.
-acc_A1_train = model_A1.train(args...) # Train model based on the training set (you should fine-tune your model based on validation set.)
-acc_A1_test = model_A1.test(args...)   # Test model based on the test set.
-Clean up memory/GPU etc...             # Some code to free memory if necessary.
+# Initialisation parameters
+split_percentage = 80 # 80% training data - 20% testing data split
+feature_type = 'landmarks' # Feature extraction type
+cv_folds = 5 # Number of cross-validation folds (k)
 
+# Train and test the model with hyper-parameter tuning
+X_all, X_train, X_test, y_all, y_train, y_test = get_split_data(landmark_features_celeba, gender_labels, split_percentage, feature_type)
+model_A1, acc_A1_train, acc_A1_test, cm_A1 = build_svm_gridcv(X_train, X_test, y_train, y_test, cv_folds)
+
+# Plot learning curve and confusion matrix
+task_A1_plot, acc_A1_val = plot_learning_curve(model_A1, 'Task A1 Learning Curve - SVM', X_train, y_train, ylim=None, cv=cv_folds,
+                        n_jobs=None, train_sizes=np.linspace(.1, 1.0, 5))
+plot_confusion_matrix(cm_A1, target_names = ['male', 'female'], title = 'Gender Confusion Matrix', cmap=plt.cm.Blues, normalize=False)
 
 # ======================================================================================================================
 # Task A2
-model_A2 = A2(args...)
-acc_A2_train = model_A2.train(args...)
-acc_A2_test = model_A2.test(args...)
-Clean up memory/GPU etc...
+# Initialisation parameters
+split_percentage = 80 # 80% training data - 20% testing data split
+feature_type = 'landmarks' # Feature extraction type
+cv_folds = 5 # Number of cross-validation folds (k)
 
+# Train and test the model with hyper-parameter tuning
+X_all, X_train, X_test, y_all, y_train, y_test = get_split_data(landmark_features_celeba, smiling_labels, split_percentage, feature_type)
+model_A2, acc_A2_train, acc_A2_test, cm_A2 = build_svm_gridcv(X_train, X_test, y_train, y_test, cv_folds)
+
+# Save learning curve and confusion matrix plot figures
+task_A2_plot, acc_A2_val = plot_learning_curve(model_A2, 'Task A2 Learning Curve - SVM', X_train, y_train, ylim=None, cv=cv_folds,
+                        n_jobs=None, train_sizes=np.linspace(.1, 1.0, 5))
+plot_confusion_matrix(cm_A2, target_names = ['no smile', 'smile'], title = 'Smile Confusion Matrix', cmap=plt.cm.Blues, normalize=False)
 
 # ======================================================================================================================
+# Data preprocessing for Task B1 and B2
+feature_type = 'rgb' # Feature extraction type
+sample_size = 10000 # Full dataset import
+img_features_cartoon_set, eye_color_labels, face_shape_labels = extract_features_labels_from_cartoon_set(feature_type, sample_size)
+# ======================================================================================================================
 # Task B1
-model_B1 = B1(args...)
-acc_B1_train = model_B1.train(args...)
-acc_B1_test = model_B1.test(args...)
-Clean up memory/GPU etc...
+# Initialisation parameters
+split_percentage = 80 # 80% training data - 20% testing data split
+feature_type = 'rgb' # Feature extraction type
+cv_folds = 5 # Number of cross-validation folds (k)
 
+# Train and test the model with hyper-parameter tuning
+X_all, X_train, X_test, y_all, y_train, y_test = get_split_data(img_features_cartoon_set, eye_color_labels, split_percentage, feature_type)
+model_B1, acc_B1_train, acc_B1_test, cm_B1 = build_svm_gridcv(X_train, X_test, y_train, y_test, cv_folds)
+
+# Save learning curve and confusion matrix plot figures
+task_B1_plot, acc_B1_val = plot_learning_curve(model_B1, 'Task B1 Learning Curve - SVM', X_train, y_train, ylim=None, cv=cv_folds,
+                        n_jobs=None, train_sizes=np.linspace(.1, 1.0, 5))
+plot_confusion_matrix(cm_B1, target_names = ['0', '1', '2', '3', '4'], title = 'Eye Colour Confusion Matrix', cmap=plt.cm.Blues, normalize=False)
 
 # ======================================================================================================================
 # Task B2
-model_B2 = B2(args...)
-acc_B2_train = model_B2.train(args...)
-acc_B2_test = model_B2.test(args...)
-Clean up memory/GPU etc...
+# Initialisation parameters
+split_percentage = 80 # 80% training data - 20% testing data split
+feature_type = 'landmarks' # Feature extraction type
+cv_folds = 5 # Number of cross-validation folds (k)
 
+# Train and test the model with hyper-parameter tuning
+X_all, X_train, X_test, y_all, y_train, y_test = get_split_data(img_features_cartoon_set, face_shape_labels, split_percentage, feature_type)
+model_B2, acc_B2_train, acc_B2_test, cm_B2 = build_svm_gridcv(X_train, X_test, y_train, y_test, cv_folds)
+
+# Save learning curve and confusion matrix plot figures
+task_B2_plot, acc_B2_val = plot_learning_curve(model_B2, 'Task B2 Learning Curve - SVM', X_train, y_train, ylim=None, cv=cv_folds,
+                        n_jobs=None, train_sizes=np.linspace(.1, 1.0, 5))
+plot_confusion_matrix(cm_B2, target_names = ['0', '1', '2', '3', '4'], title = 'Face Shape Confusion Matrix', cmap=plt.cm.Blues, normalize=False)
 
 # ======================================================================================================================
- 
-
-# If you are not able to finish a task, fill the corresponding variable with 'TBD'. For example:
-# acc_A1_train = 'TBD'
-# acc_A1_test = 'TBD'
+# Print out your results with following format:
+print('TA1:{},{},{};TA2:{},{},{};TB1:{},{},{};TB2:{},{},{};'.format(acc_A1_train, acc_A1_val, acc_A1_test,
+                                                                    acc_A2_train, acc_A2_val, acc_A2_test,
+                                                                    acc_B1_train, acc_B1_val, acc_B1_test,
+                                                                    acc_B2_train, acc_B2_val, acc_B2_test))
